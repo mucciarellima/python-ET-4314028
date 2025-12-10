44bf4c493e8dae344185ac4d362f14bb6d4a00f0
mucciarellima <44730926+mucciarellima@users.noreply.github.com>
2025-12-10 21:45:36 +0000

    marco updated exercise files with new content and corrections

--- exercise_files/ASCII_ART.py
+++ exercise_files/ASCII_ART.py
@@ -1,7 +1,9 @@
 import os
+string1 ='AAAAABBBBCCC'
 
 art = '''
-mmm
+
+
                                                                                 
                                                                                 
                                %%%%%%%%%%%%%%%%%%%                              
@@ -48,73 +50,87 @@ def clear_terminal():
         _ = os.system('clear')
 
 
-'''
-steps to encode:
-0. go through the ascii art and put each character into a set to establish the unique characters used in the artwork - to do
-    example {' ', '%', '\n'}
-1. split the ascii art into a list of strings, one for each line - done
-2. count the number of lines - done
-3. for each line, count the occurrences of each character AND store the counts in a LIST of TUPLES for each line - to do
-    example [('m', 2),(' ', 10), ('%', 5),(' ', 15), ('\n', 1)]
-4. put all the lines into a master List of Lists of Tuples - to do
-5. return the master List of Lists of Tuples - to do
-'''
-
-
 
 def encodeString(stringVal):
     # Your code goes here.
     #charlist = set(stringVal)#this creates a set of unique characters in the string that was passed in
     #print("set of characters", charlist)
+    outputList = []
+    lineTuple = ()
     prevChar=None
-    counter=1
+    counter=0
     linenumber=0
-    decodelist=[]
-    decodetuple=()    
-    lineByLine = stringVal.split('\n')
-    #linebyline23=stringVal.splitlines() this does the same thing as split('\n')
-    #print("linebyline is:", lineByLine)
-    #print("linebyline23 is:", linebyline23)
+    startChrFlag=True
+    lineByLine = stringVal.splitlines() #splits the string into a list of lines
+            #linebyline23=stringVal.splitlines() this does the same thing as split('\n')
+            #print("linebyline is:", lineByLine)
+            #print("linebyline23 is:", linebyline23)
     prevChar=None
     
-    for line in lineByLine[0:3]:
-        #print("line is:", line)
-        linenumber= linenumber + 1
-        
+    for line in lineByLine:
+        #print(f'processing line: {linenumber} .... ({line})')
+        '''if line == "" or line == None: #skips empty lines ... for now
+            #print(f"skipping empty line at L{linenumber}")
+            outputList.append(('',1))
+            outputList.append(('\n',1)) #add newline character at end of line
+            counter=0
+            linenumber= linenumber + 1 
+            continue
+        '''
+        if line == None:
+            counter=0
+            continue
+    
         for char in line:
-            if char != prevChar:#test if current char does not match previous character (leading edge detection)
-                #if current character is different than previous character then
+            charTuple= ()
+            if char != prevChar and startChrFlag != True :#test if current char does not match previous character (leading edge detection)
+                #if current character is different than previous character then log the previous character and its count
                 #remember in the first iteration prevChar is None so this will always be true
-                print(f'{linenumber}\"{char}\" repeats {counter} times')
-                counter = 1#we reset the counter becase we have a new character
+                #startChrFlag is used to skip this block for the first character processed becuase there is no previous character yet
+
+                charTuple= prevChar,counter
+                #print(f'Appending to outputList: {charTuple}')
+                outputList.append(charTuple)
+                #print(f'outputList is now: {outputList}') 
+                counter = 1 #we reset the counter becuase we have a new character
+                #print(f'L{linenumber},Ctr{counter},Ch\'{char}\' P\'{prevChar}\'<>')
+                
             else:# if char == prevChar then we have a repeat of the same character
-                counter= counter + 1 #increment the counter
+                counter += 1 #increment the counter
+                #print(f'L{linenumber},Ctr{counter},Ch\'{char}\' P\'{prevChar}\'==')
+                #print(f'test \"{prevChar}\" repeats {counter} times')
+                startChrFlag= False #make sure startChrFlag is off after first character processed
+                    
+            
             prevChar = char#advance the previous character to current character
             #so that we can perform the perform the next iteration
-                
         
-        '''
-        for char in line:
-            if char != prevChar:#test if current char does not match previous character (leading edge detection)
-                #if current character is different than previous character then
-                #remember in the first iteration prevChar is None so this will always be true
-                counter = 1#we reset the counter becase we have a new character
-            else:# if char == prevChar then we have a repeat of the same character
-                counter= counter + 1 #increment the counter
-                print(f'{linenumber}\"{char}\" repeats {counter} times')
-            prevChar = char#advance the previous character to current character
-            #so that we can perform the compare on the next iteration
-        '''
-    
-    #print("num of lines is", len(lineByLine))
-    return #lineByLine
+        linenumber= linenumber + 1
+        startChrFlag= False #after first line processed we turn off the startChrFlag
+        #end of line processing - we need to log the last character and its count
+        charTuple= prevChar,counter
+        #print(f'Appending to outputList: {charTuple}')
+        #print(f'End of line L{linenumber-1} reached. Appending new line character.')
+        outputList.append(charTuple)
+        if linenumber-1  >= 0 and linenumber < len(lineByLine):
+            outputList.append(('\n',1)) #add newline character at end of line
+        #print(f'outputList is now: {outputList}')
+        counter=0 #reset counter for next line
+        
+    return outputList
 
 
 
 
 def decodeString(encodedList):
-    # Your code goes here.
-    pass
+    inputList = encodedList
+    outputString = ''
+    for charTuple in inputList:
+        char = charTuple[0]
+        count = charTuple[1]
+        outputString += char * count #repeat the character 'count' times and append to output string 
+    return outputString
+ 
 
 
 
@@ -122,6 +138,10 @@ def decodeString(encodedList):
 clear_terminal() # Clear the terminal before printing
 
 encoded = encodeString(art)
+print(f'here is the encoded string of characters {encoded}')
+
+##decoded = decodeString(encoded)
+#print(f'here is the decoded string of characters:\n{decoded}')
 
 #print(encoded)
 
